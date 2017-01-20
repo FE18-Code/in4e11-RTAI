4c9ea12ff24cc4796f2de34622f6b2bad1cd0686
ams <ams.gh@hmamail.com>
2017-01-20 09:42:54 +0100

    tp1 - added source files

---
+++ task.c
@@ -0,0 +1,64 @@
+/*
+Squelette module RTAI
+*/
+#include <linux/module.h>
+MODULE_LICENSE("GPL");
+#include <asm/io.h>
+
+#include <asm/rtai.h>
+#include <rtai_sched.h>
+
+#define NUMERO      1
+#define PRIORITE    1
+#define STACK_SIZE  2000
+#define PERIODE     1000000000    //  1 s
+#define TICK_PERIOD 1000000    //  1 ms
+#define N_BOUCLE    10
+
+static RT_TASK ma_tache;
+
+void mon_code(int arg) {
+
+  RTIME t;
+  static int boucle = N_BOUCLE ;
+
+  while (boucle--)
+  {
+
+    /* a completer */
+  }
+}
+
+static int mon_init(void) {
+
+  int ierr;
+  RTIME now;
+
+  rt_set_oneshot_mode();
+
+  ierr = rt_task_init(&ma_tache, mon_code, NUMERO, 
+		      STACK_SIZE, PRIORITE, 0, 0);
+  printk("[tache %d] cree code retour %d par programme %s\n",
+	 NUMERO,ierr,__FILE__);
+
+  if (!ierr) {
+    
+    start_rt_timer(nano2count(TICK_PERIOD));
+
+    /* a completer */
+
+  }
+  return ierr;
+}
+
+void mon_exit(void) {
+
+  stop_rt_timer();
+  
+  rt_task_delete(&ma_tache);
+
+}
+
+
+module_init(mon_init);
+module_exit(mon_exit);
